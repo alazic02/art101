7beab8dd1114d3093358b29117040792dbdef9fb
Ariana L <alazich@ucsc.edu>
2021-10-18 19:34:38 -0700

    lab5 major overhaul
    updated index.html, lab.js, lab.css to follow assignment requirements; added red honda img

---
+++ lab5/js/lab.js
@@ -0,0 +1,23 @@
+/* UCSC; ART101 Fall 2021; Lab 5: Data Types and Variables.
+ * Experiment with basic JavaScript data types and variables.
+ * @author  Ariana Lazich and Marlene Lopez
+ * @since   19 October 2021
+*/
+
+// declare Vehicle variables
+var make = "Honda";
+var model = "Civic";
+var color = "Red";
+var year = 2012;
+var ownIt = false;
+
+// calculate Age
+var age = 2021 - year;
+
+// output; print statement is WRITE*L*N, not uppercase 'i'
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
+document.writeln("Owned: " + ownIt + "<br>");
