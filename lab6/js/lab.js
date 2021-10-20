a7f5c92ca74917ff64ebcc2c917da1ca41d03319
Ariana L <alazich@ucsc.edu>
2021-10-20 16:20:05 -0700

    finished lab
    declared array and obj in lab.js; added image to img folder; updated and formatted index.html and called print statements from lab.js; added CSS

---
+++ lab6/js/lab.js
@@ -0,0 +1,34 @@
+/* UCSC; ART101 Fall 2021; Lab 6 - Arrays and Objects.
+ * Experiment with JavaScript arrays and objects.
+ * @author  Ariana Lazich and Marlene Lopez
+ * @since   21 October 2021
+*/
+
+//array of main types of transport
+var myTransport = ["car", "bus", "train", "bike", "scooter"];
+
+//main ride object
+var myMainRide = {
+  type: myTransport[0],
+  make: 'Honda',
+  model: 'Civic',
+  color: 'Red',
+  year: 2012
+};
+//add age to myMainRide obj
+myMainRide.age = 2021 - myMainRide.year;
+
+//output
+  //prints myTransport array
+  document.writeln("Getting around: " +
+                    myTransport[0] + ", " +
+                    myTransport[1] + ", " +
+                    myTransport[2] + ", " +
+                    myTransport[3] + ", " +
+                    myTransport[4] + "<br>"
+                  );
+
+  //prints myMainRide obj in curly brace, list format
+  document.writeln("<p>My Main Ride's Specs: <pre>",
+                    JSON.stringify(myMainRide, null, '\t'),
+                    "<pre></p>");
