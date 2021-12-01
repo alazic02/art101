e9dc1500a63fc56aad8723bf4ea45025150a7b01
Ariana L <alazich@ucsc.edu>
2021-12-01 00:00:34 -0800

    basics for labs 14+15
    added index.html template and comment block for lab.js for both labs

---
+++ lab14/js/lab.js
@@ -0,0 +1,5 @@
+/* UCSC; ART101 Fall 2021; Lab 14 - Debugging Tools & Strategies.
+ * Use current debugging knowledge to fix old assignments.
+ * @author  Ariana Lazich
+ * @since   30 November 2021
+*/
---
+++ lab15/js/lab.js
@@ -0,0 +1,5 @@
+/* UCSC; ART101 Fall 2021; Lab 15 - AJAX.
+ * Experiment with jQuery and AJAX.
+ * @author  Ariana Lazich
+ * @since   30 November 2021
+*/
