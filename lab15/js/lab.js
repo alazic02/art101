05612bdba07c0fe722d7d42e498220c4ae35cc83
Ariana L <alazich@ucsc.edu>
2021-12-03 20:03:18 -0800

    Update lab.js
    replaced numbersapi with dinosaur facts; issue with https

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -4,41 +4,25 @@
  * @since   30 November 2021
 */
 
-// event listener for random number trivia
-$("#num-triv").click(function() {
-  console.log("number trivia was clicked!"); // test
-  callNumTriva();
-});
-// event listener for random date fact
-$("#date-fact").click(function() {
-  console.log("date factoid was clicked!"); // test
-  callDateFact();
+// event listener for random dino fact
+$("#activate").click(function() {
+  console.log("button was clicked!"); // test
+  callDino();
 });
 
-// function to call AJAX and get random number trivia
-function callNumTriva() {
-  // using core $.ajax() method
+// function that calls AJAX and gets a dinosaur fact
+function callDino() {
   $.ajax({
-    // API endpoint
-    url: "http://numbersapi.com/random",
-    // any data to send
-    //data: {id: "random"},
-    // POST or GET request
+    url: "https://dinosaur-facts-api.shultzlab.com/dinosaurs/random",
     type: "GET",
-    // data type to expect back
-    //dataType: "json",
-      /* kept getting a 'parsererror' so I looked on Stackoverflow and was told
-       * to remove the expected data type which fixed it and let me call from
-       * numbersapi; see:
-       * https://stackoverflow.com/questions/5061310/jquery-returning-parsererror-for-ajax-request
-      */
+    dataType: "json",
   })
 
-  // request succeeds
+  //request succeeds
   .done(function(data) {
     console.log("Success!"); // test
     // add data to 'output' div
-    $("#output").html("<p>" + data + "</p>");
+    $("#output").html("<p>" + JSON.stringify(data) + "</p>");
   })
   // request fails
   .fail(function(request, error) {
@@ -46,20 +30,68 @@ function callNumTriva() {
   })
 }
 
-// function to call AJAX and get random date trivia
-function callDateFact() {
-  $.ajax({
-    url: "http://numbersapi.com/random/date",
-    type: "GET",
-  })
-  // request succeeds
-  .done(function(data) {
-    console.log("Success!"); // test
-    // add data to 'output' div
-    $("#output").html("<p>" + data + "</p>");
-  })
-  // request fails
-  .fail(function(request, error) {
-    console.log("Something went wrong: ", request, error); // test
-  })
-}
+/* I originally was using the numbersapi that was provided for us in the lecture,
+ * however it stopped working once it was pushed to GitHub because it is http
+ * while GitHub is https. below is the code I tinkered with to learn how to call
+ * AJAX and load API:
+ *
+ *  // event listener for random number trivia
+ *  $("#num-triv").click(function() {
+ *    console.log("number trivia was clicked!"); // test
+ *    callNumTriva();
+ *  });
+ *  // event listener for random date fact
+ *  $("#date-fact").click(function() {
+ *    console.log("date factoid was clicked!"); // test
+ *    callDateFact();
+ *  });
+ *
+ *  // function to call AJAX and get random number trivia
+ *  function callNumTriva() {
+ *      // using core $.ajax() method
+ *      $.ajax({
+ *        // API endpoint
+ *        url: "http://numbersapi.com/random",
+ *        // any data to send
+ *        //data: {id: "random"},
+ *        // POST or GET request
+ *        type: "GET",
+ *        // data type to expect back
+ *        //dataType: "json",
+ *          /* kept getting a 'parsererror' so I looked on Stackoverflow and was told
+ *           * to remove the expected data type which fixed it and let me call from
+ *           * numbersapi; see:
+ *           * https://stackoverflow.com/questions/5061310/jquery-returning-parsererror-for-ajax-request
+ *          */
+ /*      })
+ *
+ *    // request succeeds
+ *      .done(function(data) {
+ *        console.log("Success!"); // test
+ *        // add data to 'output' div
+ *        $("#output").html("<p>" + data + "</p>");
+ *      })
+ *      // request fails
+ *      .fail(function(request, error) {
+ *        console.log("Something went wrong: ", request, error); // test
+ *      })
+ *    }
+ *
+ *    // function to call AJAX and get random date trivia
+ *    function callDateFact() {
+ *      $.ajax({
+ *        url: "http://numbersapi.com/random/date",
+ *        type: "GET",
+ *      })
+ *      // request succeeds
+ *      .done(function(data) {
+ *        console.log("Success!"); // test
+ *        // add data to 'output' div
+ *        $("#output").html("<p>" + data + "</p>");
+ *      })
+ *      // request fails
+ *      .fail(function(request, error) {
+ *        console.log("Something went wrong: ", request, error); // test
+ *      })
+ *    }
+*/
