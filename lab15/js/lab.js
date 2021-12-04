f6c5851795d335fcc7f1856e750a1601c0ad8bb0
Ariana L <alazich@ucsc.edu>
2021-12-03 18:17:14 -0800

    lab works
    added AJAX calls that use numbersapi to generate random number trivia and random facts about dates; added buttons to the HTML and corresponding event listeners in the JS; AJAX-calling functions add the info to the 'output' div on the index.html

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -3,3 +3,63 @@
  * @author  Ariana Lazich
  * @since   30 November 2021
 */
+
+// event listener for random number trivia
+$("#num-triv").click(function() {
+  console.log("number trivia was clicked!"); // test
+  callNumTriva();
+});
+// event listener for random date fact
+$("#date-fact").click(function() {
+  console.log("date factoid was clicked!"); // test
+  callDateFact();
+});
+
+// function to call AJAX and get random number trivia
+function callNumTriva() {
+  // using core $.ajax() method
+  $.ajax({
+    // API endpoint
+    url: "http://numbersapi.com/random",
+    // any data to send
+    //data: {id: "random"},
+    // POST or GET request
+    type: "GET",
+    // data type to expect back
+    //dataType: "json",
+      /* kept getting a 'parsererror' so I looked on Stackoverflow and was told
+       * to remove the expected data type which fixed it and let me call from
+       * numbersapi; see:
+       * https://stackoverflow.com/questions/5061310/jquery-returning-parsererror-for-ajax-request
+      */
+  })
+
+  // request succeeds
+  .done(function(data) {
+    console.log("Success!"); // test
+    // add data to 'output' div
+    $("#output").html("<p>" + data + "</p>");
+  })
+  // request fails
+  .fail(function(request, error) {
+    console.log("Something went wrong: ", request, error); // test
+  })
+}
+
+// function to call AJAX and get random date trivia
+function callDateFact() {
+  $.ajax({
+    url: "http://numbersapi.com/random/date",
+    type: "GET",
+  })
+  // request succeeds
+  .done(function(data) {
+    console.log("Success!"); // test
+    // add data to 'output' div
+    $("#output").html("<p>" + data + "</p>");
+  })
+  // request fails
+  .fail(function(request, error) {
+    console.log("Something went wrong: ", request, error); // test
+  })
+}
